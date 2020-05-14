d6c013bea0701ecdcdc6a309e3d2c1c673cda6e5
DavisVaughan <davis@rstudio.com>
2020-05-14 17:04:10 -0400

    Add additional helpers

--- src/utils.c
+++ src/utils.c
@@ -1,1 +1,29 @@
 #include "utils.h"
+
+// -----------------------------------------------------------------------------
+
+#define BUFSIZE 8192
+
+#define INTERP(BUF, FMT, DOTS) {        \
+  va_list dots;                         \
+  va_start(dots, FMT);                  \
+  vsnprintf(BUF, BUFSIZE, FMT, dots);   \
+  va_end(dots);                         \
+                                        \
+  BUF[BUFSIZE - 1] = '\0';              \
+}
+
+// [[ include("utils.h") ]]
+void r_abort(const char* fmt, ...) {
+  char buf[BUFSIZE];
+  INTERP(buf, fmt, ...);
+
+  Rf_errorcall(R_NilValue, buf);
+
+  never_reached("r_abort");
+}
+
+#undef BUFSIZE
+#undef INTERP
+
+// -----------------------------------------------------------------------------
--- src/utils.h
+++ src/utils.h
@@ -17,4 +17,40 @@ static inline enum r_type r_typeof(sexp x) {
 
 // -----------------------------------------------------------------------------
 
+static inline sexp r_new_vector(enum r_type type, r_ssize size) {
+  return Rf_allocVector(type, size);
+}
+
+// -----------------------------------------------------------------------------
+
+static inline sexp r_list_get(sexp x, r_ssize i) {
+  return VECTOR_ELT(x, i);
+}
+static inline sexp r_list_set(sexp x, r_ssize i, sexp value) {
+  return SET_VECTOR_ELT(x, i, value);
+}
+
+// -----------------------------------------------------------------------------
+
+static inline double* r_dbl_deref(sexp x) {
+  return REAL(x);
+}
+
+
+static inline const double* r_dbl_const_deref(sexp x) {
+  return (const double*) REAL(x);
+}
+
+// -----------------------------------------------------------------------------
+
+__attribute__((noreturn)) static inline void never_reached(const char* fn) {
+  Rf_errorcall(r_null, "Internal error: Reached the unreachable in `%s()`.", fn);
+}
+
+// -----------------------------------------------------------------------------
+
+void r_abort(const char* fmt, ...);
+
+// -----------------------------------------------------------------------------
+
 #endif
