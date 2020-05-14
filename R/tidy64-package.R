f3a3e5316a31df3e310a75e1dda62a82c46a8af3
DavisVaughan <davis@rstudio.com>
2020-05-14 13:28:39 -0400

    Set up imports and package doc

---
+++ R/tidy64-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+#' @import rlang
+#' @import vctrs
+#' @importFrom glue glue
+## usethis namespace: end
+NULL
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(tidy64)
+
+test_check("tidy64")
