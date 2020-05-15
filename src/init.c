12c6444e073fda1d1e21c34350e5ba654e7d9775
DavisVaughan <davis@rstudio.com>
2020-05-15 08:03:31 -0400

    Expose R hooks for pack/unpack

---
+++ src/init.c
@@ -0,0 +1,20 @@
+#include <R.h>
+#include <Rinternals.h>
+#include <stdlib.h> // for NULL
+#include <stdbool.h> // for bool
+#include <R_ext/Rdynload.h>
+
+#include "exports.h"
+
+static const R_CallMethodDef CallEntries[] = {
+  // #include "pack.h"
+  {"export_tidy64_pack",        (DL_FUNC) &export_tidy64_pack, 1},
+  {"export_tidy64_unpack",      (DL_FUNC) &export_tidy64_unpack, 1},
+
+  {NULL, NULL, 0}
+};
+
+void R_init_tidy64(DllInfo *dll) {
+  R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+  R_useDynamicSymbols(dll, FALSE);
+}
