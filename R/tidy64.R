d7ffd069f8a8c7d7e656ee8a5c9d7b6c1c3b24d5
DavisVaughan <davis@rstudio.com>
2020-05-22 15:58:46 -0400

    Document export and test `new_tidy64()` and `new_tidy64_subclass()`

--- R/tidy64.R
+++ R/tidy64.R
@@ -2,6 +2,67 @@ tidy64 <- function(x = double()) {
   vec_cast(x, tidy64_global_empty_tidy64)
 }
 
+# ------------------------------------------------------------------------------
+
+#' Construct a new tidy64
+#'
+#' @description
+#' _For expert use only!_
+#'
+#' `new_tidy64()` is a bare bones constructor for
+#' creating tidy64 objects from their 32 bit components. You should almost
+#' always use [tidy64()] or [as_tidy64()] instead.
+#'
+#' `new_tidy64_subclass()` is a slightly more developer friendly way to create
+#' a subclass of tidy64. It takes an existing tidy64, breaks it apart, and
+#' then uses `new_tidy64()` to form a new tidy64 with the subclass and
+#' attributes attached.
+#'
+#' @param left `[double]`
+#'
+#'   A vector of integerish double values in the range of
+#'   `[-2147483648, 2147483647]`. These represent the left most 32 bits
+#'   of the `int64_t` extracted by:
+#'
+#'   - Mapping the `int64_t` to `uint64_t`.
+#'
+#'   - Right shifting with `x_u64 >> 32` to get the left 32 bits as a
+#'     `uint32_t`.
+#'
+#'   - Mapping the unsigned left 32 bits of `x_u64` to a signed `int32_t`. This
+#'     maintains ordering automatically in operations like `vec_order()`.
+#'
+#'   - Coercing the resulting `int32_t` to a double.
+#'
+#' @param right `[double]`
+#'
+#'   A vector of integerish double values in the range of
+#'   `[0, 4294967295]`. These represent the right most 32 bits
+#'   of the `int64_t` extracted by:
+#'
+#'   - Mapping the `int64_t` to `uint64_t`.
+#'
+#'   - Directly coercing the resulting `x_u64` to `uint32_t`, which gives us
+#'     the right 32 bits as a `uint32_t`.
+#'
+#'   - Coercing the resulting `uint32_t` to a double.
+#'
+#' @param ... `[named dots]`
+#'
+#'   Name value pairs resulting in extra attributes attached to the new tidy64.
+#'
+#' @param class `[character]`
+#'
+#'   A vector of subclasses.
+#'
+#' @export
+#' @examples
+#' new_tidy64()
+#' new_tidy64(0, 1)
+#' new_tidy64(0, 2)
+#' new_tidy64(1, 0)
+#'
+#' new_tidy64_subclass(tidy64(1), names = "x", class = "foobar")
 new_tidy64 <- function(left = double(), right = double(), ..., class = character()) {
   if (!is_double(left)) {
     abort("`left` must be a double.")
@@ -20,6 +81,45 @@ new_tidy64 <- function(left = double(), right = double(), ..., class = character
   )
 
   new_rcrd(fields, ..., class = c(class, "tidy64"))
+}
+
+#' @param x `[tidy64]`
+#'
+#'   An existing tidy64 to create the subclass from.
+#'
+#' @param names `[NULL / character]`
+#'
+#'   Names to attach to the new tidy64 subclass. Either `NULL` for no names,
+#'   or a character vector of names the same size as `x`.
+#'
+#' @rdname new_tidy64
+#' @export
+new_tidy64_subclass <- function(x = new_tidy64(),
+                                ...,
+                                names = NULL,
+                                class = character()) {
+  if (!is_tidy64(x)) {
+    abort("`x` must be a <tidy64>.")
+  }
+
+  names <- check_names(names, x)
+
+  left <- field(x, "left")
+  right <- field(x, "right")
+
+  new_tidy64(
+    left = left,
+    right = right,
+    `tidy64:::names` = names,
+    ...,
+    class = class
+  )
+}
+
+# ------------------------------------------------------------------------------
+
+is_tidy64 <- function(x) {
+  inherits(x, "tidy64")
 }
 
 # ------------------------------------------------------------------------------
@@ -47,6 +147,10 @@ check_names <- function(names, x) {
 
   if (vec_size(x) != vec_size(names)) {
     abort("A character vector of names must be the same size as `x`.")
+  }
+
+  if (anyNA(names)) {
+    abort("Names must not be `NA`.")
   }
 
   names
--- tests/testthat/test-tidy64.R
+++ tests/testthat/test-tidy64.R
@@ -1,4 +1,49 @@
 # ------------------------------------------------------------------------------
+# new_tidy64()
+
+test_that("can construct tidy64 from bare bones", {
+  expect_s3_class(new_tidy64(), "tidy64")
+  expect_identical(new_tidy64(), tidy64())
+})
+
+test_that("can construct tidy64 with bits", {
+  expect_identical(new_tidy64(0, 0), tidy64(0))
+  expect_identical(new_tidy64(0, 1), tidy64(1))
+})
+
+test_that("`left` and `right` are validated", {
+  expect_error(new_tidy64(left = "x"), "must be a double")
+  expect_error(new_tidy64(right = "x"), "must be a double")
+
+  expect_error(new_tidy64(left = 1, right = c(1, 1)), "same size")
+})
+
+# ------------------------------------------------------------------------------
+# new_tidy64_subclass()
+
+test_that("default is equivalent to new_tidy64()", {
+  expect_identical(new_tidy64_subclass(), new_tidy64())
+})
+
+test_that("can add a subclass", {
+  expect_s3_class(new_tidy64_subclass(class = "foobar"), "foobar")
+})
+
+test_that("can add names", {
+  expect_named(new_tidy64_subclass(tidy64(1), names = "x"), "x")
+})
+
+test_that("names are validated", {
+  expect_error(new_tidy64_subclass(tidy64(1), names = 1), "or a character vector")
+  expect_error(new_tidy64_subclass(tidy64(1), names = c("x", "y")), "same size as `x`")
+  expect_error(new_tidy64_subclass(tidy64(1), names = NA_character_), "must not be `NA`")
+})
+
+test_that("x is validated", {
+  expect_error(new_tidy64_subclass(1), "must be")
+})
+
+# ------------------------------------------------------------------------------
 # names() / `names<-`()
 
 test_that("can have NULL names", {
