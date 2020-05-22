306f4d8ab4000259d16766423ec9c8c881548094
DavisVaughan <davis@rstudio.com>
2020-05-22 13:22:34 -0400

    Fix delayedAssign typo

--- R/globals.R
+++ R/globals.R
@@ -7,7 +7,7 @@ delayedAssign("tidy64_global_int_max", .Machine$integer.max)
 delayedAssign("tidy64_global_int_min", -.Machine$integer.max)
 
 delayedAssign("tidy64_global_max", as_tidy64(tidy64_global_max_as_chr()))
-delayedAssign("tidy64_global_min", as_tidy64(tidy64_global_max_as_chr()))
+delayedAssign("tidy64_global_min", as_tidy64(tidy64_global_min_as_chr()))
 
 delayedAssign("tidy64_global_max_lossless_double_chr", "9007199254740992") # 2^53
 delayedAssign("tidy64_global_min_lossless_double_chr", "-9007199254740992") # -2^53
